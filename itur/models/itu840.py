adding print statements to ensure lat and lon used in the calculation are correct.

itur/models/itu840.py
@@ -30,7 +30,7 @@ def __fcn_columnar_content_reduced_liquid__(Lred, lat, lon, p):
         p_below = available_p[idx]
         p_above = available_p[idx + 1]
 
-        print('P is {}, P_below is {}, P_above is {}'.format(p,p_below, p_above))
+        # print('P is {}, P_below is {}, P_above is {}'.format(p,p_below, p_above))
 
     # Compute the values of Lred_a
     Lred_a = Lred(lat, lon, p_above)
@@ -692,6 +692,7 @@ def columnar_content_reduced_liquid(lat, lon, p):
     lon = prepare_input_array(lon)
     lon = np.mod(lon, 360)
     val = __model.columnar_content_reduced_liquid(lat, lon, p)
+    print('lat used: {}, lon used: {}'.format(lat,lon))
     return prepare_output_array(val, type_output) * u.kg / u.m**2
 
 
@@ -809,6 +810,7 @@ def lognormal_approximation_coefficient(lat, lon):
     lat = prepare_input_array(lat)
     lon = prepare_input_array(lon)
     lon = np.mod(lon, 360)
+    print('lognormal lat: {}, lon: {}'.format(lat,lon))
     val = __model.lognormal_approximation_coefficient(lat, lon)
     u_adim = u.dimensionless_unscaled
     return (prepare_output_array(val[0], type_output) * u_adim,