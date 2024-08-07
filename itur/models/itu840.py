d0502991f082d137719158ee4af4b08ce64d00a5
raymarke <raymarke@gmail.com>
2024-08-06 22:25:31 -0500

    added 100.0 as a ps

--- itur/models/itu840.py
+++ itur/models/itu840.py
@@ -17,7 +17,7 @@ from itur.utils import (dataset_dir, prepare_input_array, prepare_output_array,
 def __fcn_columnar_content_reduced_liquid__(Lred, lat, lon, p):
     available_p = np.array(
         [0.01, 0.02, 0.03, 0.05, 0.1, 0.2, 0.3, 0.5, 1.0, 2.0, 3.0, 5.0, 10.0, 20.0, 30.0, 50.0,
-         60.0, 70.0, 80.0, 90.0, 95.0, 99.0])
+         60.0, 70.0, 80.0, 90.0, 95.0, 99.0, 100.0])
 
     if p in available_p:
         p_below = p_above = p
@@ -128,7 +128,7 @@ class _ITU840_9_():
     def Lred(self, lat, lon, p):
         if not self._Lred:
             ps = [0.1, 0.01, 0.2, 0.02, 0.3, 0.03, 0.5, 0.05, 1, 2, 3, 5, 10, 20, 30,
-                  50, 60, 70, 80, 90, 95, 99]
+                  50, 60, 70, 80, 90, 95, 99, 100.0]
             d_dir = os.path.join(dataset_dir, '840/v8_lred_%s.npz')
             for p_load in ps:
                 self._Lred[float(p_load)] = load_data_interpolator(
@@ -199,7 +199,7 @@ class _ITU840_8_():##fix this later (monkeyed with it instead of creating new cl
     def Lred(self, lat, lon, p):
         if not self._Lred:
             ps = [0.1, 0.01, 0.2, 0.02, 0.3, 0.03, 0.5, 0.05, 1, 2, 3, 5, 10, 20, 30,
-                  50, 60, 70, 80, 90, 95, 99]
+                  50, 60, 70, 80, 90, 95, 99, 100.0]
             d_dir = os.path.join(dataset_dir, '840/v8_lred_%s.npz')
             for p_load in ps:
                 self._Lred[float(p_load)] = load_data_interpolator(
