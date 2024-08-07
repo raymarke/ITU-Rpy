ad58cdc33bba0cc85d7b9348cfb8db7230a8c119
raymarke <raymarke@gmail.com>
2024-08-07 12:00:20 -0500

    error in print statement

--- itur/models/itu840.py
+++ itur/models/itu840.py
@@ -30,7 +30,7 @@ def __fcn_columnar_content_reduced_liquid__(Lred, lat, lon, p):
         p_below = available_p[idx]
         p_above = available_p[idx + 1]
 
-        print('P is {}, P_below is {}, P_above is {}'.format(,p,p_below, p_above)
+        print('P is {}, P_below is {}, P_above is {}'.format(p,p_below, p_above)
 
     # Compute the values of Lred_a
     Lred_a = Lred(lat, lon, p_above)
